acbad69d7be237f7090218a4bb5b77c34fdc8cb5
code810 <nadirssh@code.edu.az>
2023-11-16 14:37:04 +0400

    RegexAndMatch

--- AccessModifiers/Program.cs
+++ AccessModifiers/Program.cs
@@ -1,7 +1,40 @@
 ﻿using GunSimulator.Helpers;
-
+using System.Text.RegularExpressions;
 //GunShoting start=new ();
 //start.StartGunShooting();
 
 GunShoting2 start = new();
 start.StartGunShooting();
+#region Regex test
+//string text = "Loremipsum";
+//Regex regex = new("^Lo");
+//Console.WriteLine(regex.Match(text));
+//Match result = regex.Match(text);
+//Console.WriteLine(result.Success);
+//Regex regex2 = new("ip");
+//Console.WriteLine(regex2.Match(text));
+//Match result2 = regex2.Match(text);
+//Console.WriteLine(result2.Success);
+
+//string input = Console.ReadLine();
+//Regex test = new(@"^\+994(77|70|50|51|55|99)[0-9]{7}$");
+//Match result = test.Match(input);
+//if (result.Success)
+//{
+//    Console.WriteLine("Nomre duzgun daxil edildi");
+//}
+//else
+//{
+//    Console.WriteLine("Sehfdir");
+//}
+
+//string text = "Loremipsum";
+//Regex regex = new("Lorem");
+//Console.WriteLine(regex.Replace(text,"MEROL"));
+
+
+
+
+#endregion
+
+
